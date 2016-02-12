dbc28acc7bfcb9f902fa8014c3ce2ba79abec251
vita <vicente.tarin@mimacom.com>
2016-02-12 13:03:30 +0100

    swagger

---
+++ Web Administration Module/src/main/java/com/bb/manager/web/AdminController.java
@@ -0,0 +1,20 @@
+package com.bb.manager.web;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/admin", produces = "application/json")
+public class AdminController {
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void ping() {
+    }
+
+
+}
